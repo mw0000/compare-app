7c0d26fe4c6e80128de3bbf77d6060f15e4275e4
mw0000 <m@wilkowski.org>
2018-03-18 00:59:22 +0100

    side menu

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,12 +3,12 @@
 $(function () {
     $("#switchRoundedInfo").click(function () {
         if ($(this).is(":checked")) {
-            $(".lang-menu").show();
+            $(".lang-menu").fadeIn("slow");
             $('#main').addClass('is-three-quarters');
             $('#main').addClass('column');
             $("#columns").attr('class', 'columns');
         } else {
-            $(".lang-menu").hide();
+            $(".lang-menu").fadeOut("slow");
             $('#main').removeClass('is-three-quarters');
             $('#main').removeClass('column');
             $("#columns").attr('class', 'column');
